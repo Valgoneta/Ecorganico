885a5c2cffa8242364758ca1572103930e146944
Valenmar03 <valenmrtnz8@gmail.com>
2022-06-29 23:57:34 -0300

    Dinamismo y diseño en cards de carrito

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
-console.log(listaProductos);
+let carritoCompras = [];
+
+const productsContainer = document.getElementById('contenedor-productos');
+
+
+
+mostrarProductos()
+
+
+function mostrarProductos(){
+    
+    listaProductos.forEach(el => {
+
+        let div = document.createElement('div')
+        div.className = 'cards-container'
+
+        div.innerHTML = `<div class="cards">
+                            <div class="card-image">
+                                <img src="${el.img}" width="100px">
+                            </div>
+                            <div class="card-values">
+                                <span class="card-title">${el.nombre}</span>
+                                <p class="precio"> $${el.precio}</p>
+                                <button class="boton">Agregar al carrito</button>
+                            </div>
+                        </div> `
+
+                    
+        productsContainer.appendChild(div)
+        
+    })
+}
+
 
 
 
